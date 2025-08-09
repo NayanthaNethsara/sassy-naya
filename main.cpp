1ad70d8a9de6de397f84f4b8b5f9255232747c2d
NayanthaNethsara <nayanthanethsara@gmail.com>
2025-08-09 11:26:47 +0530

    Refactor main.cpp to improve error handling and update window title; streamline event processing and rendering

--- main.cpp
+++ main.cpp
@@ -1,62 +1,55 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow(
-        "Hello SDL",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Chalithaya - SDL2 Test",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          800, 600, SDL_WINDOW_SHOWN);
 
-    if (!window)
+    if (window == nullptr)
     {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+                                                SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == nullptr)
     {
-        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
 
-    bool running = true;
-    SDL_Event event;
+    bool quit = false;
+    SDL_Event e;
 
-    while (running)
+    while (!quit)
     {
-        // Process events
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&e))
         {
-            if (event.type == SDL_QUIT)
-            {
-                running = false;
-            }
+            if (e.type == SDL_QUIT)
+                quit = true;
         }
 
-        // Draw
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black background
+        SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255); // Blue color
         SDL_RenderClear(renderer);
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red line
-        SDL_RenderDrawLine(renderer, 100, 100, 700, 500);
-
         SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
